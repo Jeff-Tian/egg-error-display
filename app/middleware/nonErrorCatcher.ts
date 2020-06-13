dda383c7e0dbcedf0cbc9037d1531fd5ebe72e89
Jeff Tian <jeff.tian@outlook.com>
2020-06-13 13:29:26 +0800

    fix: inspect err

--- app/middleware/nonErrorCatcher.ts
+++ app/middleware/nonErrorCatcher.ts
@@ -1,20 +1,21 @@
-import {Context} from 'egg';
+import {Context} from 'egg'
+import util from 'util'
 
 export default () => async (_ctx: Context, next: () => Promise<void>) => {
     try {
-        await next();
+        await next()
     } catch (err) {
         if (!(err instanceof Error) && typeof err === 'object') {
-            const error: any = new Error('non-error thrown: ' + err)
+            const error: any = new Error('non-error thrown: ' + err + '\n' + util.inspect(err))
 
             for (const key in err) {
                 error[key] = err[key]
             }
 
-            throw error;
+            throw error
         }
 
-        throw err;
+        throw err
     }
 }
 
